36a572ec4c87307c5e25ad5e4f8b38d2dc858344
Mr Tomato <58458857+mrTomatolegit@users.noreply.github.com>
2021-09-08 22:56:26 +0300

    add invite command

---
+++ src/commands/invite.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('discord.js').ApplicationCommandData}
+ */
+module.exports = {
+    description: 'Invite the bot to another server!',
+    /**
+     *
+     * @param {import("discord.js").Client} client
+     * @param {import("discord.js").CommandInteraction} i
+     */
+    run: async (client, i) => {
+        i.reply({
+            content: `[Invite link](${client.generateInvite({
+                scopes: ['applications.commands']
+            })})`,
+            ephemeral: true
+        });
+    }
+};
