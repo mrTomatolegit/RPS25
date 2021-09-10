b886bb2d44359580899b94431eae9527686f312a
Mr Tomato <58458857+mrTomatolegit@users.noreply.github.com>
2021-09-10 09:31:50 +0300

    add bot scope to invite

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -11,7 +11,7 @@ module.exports = {
     run: async (client, i) => {
         i.reply({
             content: `[Invite link](${client.generateInvite({
-                scopes: ['applications.commands']
+                scopes: ['applications.commands', 'bot']
             })})`,
             ephemeral: true
         });
